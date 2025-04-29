0e387119f0cf02ef5e86b2a0d161c05cd267f142
Darren Fuller <darren@elastacloud.com>
2025-04-29 10:17:57 +0100

    Add example usage for embedding prompt files
    Added two detailed examples demonstrating how to use a Manager with embedded prompt files. These showcase generating system and user prompts using input parameters and embedded resources.

--- manager_test.go
+++ manager_test.go
@@ -1,10 +1,14 @@
 package dotprompt
 
 import (
+	"embed"
 	"errors"
 	"fmt"
 	"testing"
 )
+
+//go:embed prompts
+var promptFiles embed.FS
 
 type MockLoader struct {
 	PromptFiles []PromptFile
@@ -161,6 +165,88 @@ func TestGetPromptFile_WithInvalidPromptName(t *testing.T) {
 	if promptError.Error() != expectedError {
 		t.Fatalf("Expected error %s, got %s", expectedError, promptError.Error())
 	}
+}
+
+// Example demonstrates the process of using a Manager to load and generate prompts from a specified prompt file.
+func Example() {
+	// Create a new manager instance, this will default to loading prompt files which are in the `prompts` folder
+	// in the current working directory
+	mgr, err := NewManager()
+	if err != nil {
+		panic(err)
+	}
+
+	// Load the prompt file from the manager
+	promptFile, err := mgr.GetPromptFile("example")
+	if err != nil {
+		panic(err)
+	}
+
+	// Define the values for the prompt input parameters
+	parameters := map[string]interface{}{
+		"topic": "bluetooth",
+		"style": "used car salesperson",
+	}
+
+	// Generate the system and user prompts
+	systemPrompt, _ := promptFile.GetSystemPrompt(parameters)
+	userPrompt, _ := promptFile.GetUserPrompt(parameters)
+
+	fmt.Println("System prompt:")
+	fmt.Println(systemPrompt)
+	fmt.Println("\nUser prompt:")
+	fmt.Println(userPrompt)
+
+	// Output:
+	// System prompt:
+	// You are a helpful research assistant who will provide descriptive responses for a given topic and how it impacts society
+	//
+	// User prompt:
+	// Explain the impact of bluetooth on how we engage with technology as a society
+	// Can you answer in the style of a used car salesperson
+}
+
+// Example_withEmbeddedPrompts demonstrates the process of loading a file with embedded prompts and generating outputs.
+// It uses a Manager to retrieve the prompt file and generate system/user prompts based on input parameters.
+func Example_withEmbeddedPrompts() {
+	// promptFiles is of type `embed.FS`
+	// see: https://pkg.go.dev/embed
+	store := NewFSStore(promptFiles)
+
+	// Load the prompt file from the manager
+	mgr, err := NewManagerFromLoader(store)
+	if err != nil {
+		panic(err)
+	}
+
+	// Load the prompt file from the manager
+	promptFile, err := mgr.GetPromptFile("example")
+	if err != nil {
+		panic(err)
+	}
+
+	// Define the values for the prompt input parameters
+	parameters := map[string]interface{}{
+		"topic": "bluetooth",
+		"style": "used car salesperson",
+	}
+
+	// Generate the system and user prompts
+	systemPrompt, _ := promptFile.GetSystemPrompt(parameters)
+	userPrompt, _ := promptFile.GetUserPrompt(parameters)
+
+	fmt.Println("System prompt:")
+	fmt.Println(systemPrompt)
+	fmt.Println("\nUser prompt:")
+	fmt.Println(userPrompt)
+
+	// Output:
+	// System prompt:
+	// You are a helpful research assistant who will provide descriptive responses for a given topic and how it impacts society
+	//
+	// User prompt:
+	// Explain the impact of bluetooth on how we engage with technology as a society
+	// Can you answer in the style of a used car salesperson
 }
 
 // ExampleNewManager demonstrates the process of creating a new Manager instance which loads from the default "prompts"
